ac49673242fd9bb9e65aff346f1a45b001c8ca7a
srtcdmry <74022745+srtcdmry@users.noreply.github.com>
2021-09-22 16:09:35 +0300

    Initial commit

---
+++ app/src/main/java/com/info/retrofitexample/MainActivity.java
@@ -0,0 +1,209 @@
+package com.info.retrofitexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textViewResult;
+    private JsonPlaceHolderApi jsonPlaceHolderApi;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewResult = findViewById(R.id.text_view_result);
+
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build();
+
+        Gson gson = new GsonBuilder().serializeNulls().create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                //.addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(okHttpClient) // burası http eklendikten sonra
+                .build();
+
+        jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+
+       // getPosts();
+        //getComments();
+        //createPost();
+        updatePost();
+        //deletePost();
+    }
+
+    private void getPosts(){
+
+        Map<String,String> parameters = new HashMap<>();
+        parameters.put("userId","1");
+        parameters.put("_sort","id");
+        parameters.put("_order","desc");
+       // Call<List<Post>> call = jsonPlaceHolderApi.getPosts(1,4,"id","desc");
+       // Call<List<Post>> call = jsonPlaceHolderApi.getPosts(new Integer[]{2,3,6},"id","desc");  // userİd dizi olarak tanımlandığında
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(parameters); // hashmap ile
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if(!response.isSuccessful()){
+                    textViewResult.setText("Code: "+response.code());
+                    return ;
+                }
+                List<Post> posts = response.body();
+
+                for(Post post : posts){
+                    String content = "";
+                    content +="ID: "+post.getId() + "\n";
+                    content+="User ID: "+post.getUserId() + "\n";
+                    content+="Title: "+post.getTitle()+ "\n";
+                    content+="Text: "+post.getText()+"\n\n";
+
+                    textViewResult.append(content);
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void getComments(){
+       // Call<List<Comment>> call = jsonPlaceHolderApi.getComments(3);
+        Call<List<Comment>> call = jsonPlaceHolderApi.getComments("posts/3/comments"); // url ile çağırma
+
+        call.enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+                if(!response.isSuccessful()){
+                    textViewResult.setText("Code: "+response.code());
+                    return;
+                }
+
+                List<Comment> comments = response.body();
+
+                for(Comment comment : comments){
+                    String content = "";
+                    content +="ID: "+comment.getId() + "\n";
+                    content+="Post ID: "+comment.getPostId() + "\n";
+                    content+="Name: "+comment.getName()+ "\n";
+                    content+="Email: "+comment.getEmail()+"\n";
+                    content+="Text: "+comment.getText()+"\n\n";
+
+                    textViewResult.append(content);
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Comment>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void createPost(){
+        Map<String,String> fields = new HashMap<>();
+        fields.put("userId","25");
+        fields.put("title","New Title");
+        fields.put("body","New Text");
+        //Post post = new Post(23,"New Title","New Text");
+       // Call<Post> call = jsonPlaceHolderApi.createPost(post);
+        //Call<Post> call = jsonPlaceHolderApi.createPost(23,"New Title","New Text");  //formurlenco tanımlaması için
+        Call<Post> call = jsonPlaceHolderApi.createPost(fields);  // fields tanımlaması için geçerli
+        call.enqueue(new Callback<Post>() {
+            @Override
+            public void onResponse(Call<Post> call, Response<Post> response) {
+                if(!response.isSuccessful()){
+                    textViewResult.setText("Code: "+response.code());
+                    return;
+                }
+
+                Post postResponse = response.body();
+                String content = "";
+                content+="Code: "+response.code()+"\n";  // code 201 Created üretir
+                content +="ID: "+postResponse.getId() + "\n";
+                content+="User ID: "+postResponse.getUserId() + "\n";
+                content+="Title: "+postResponse.getTitle()+ "\n";
+                content+="Text: "+postResponse.getText()+"\n\n";
+
+                textViewResult.setText(content);
+            }
+
+            @Override
+            public void onFailure(Call<Post> call, Throwable t) {
+
+            }
+        });
+
+    }
+
+    private void updatePost(){
+        Post post = new Post(12,null,"New Text");
+        Call<Post> call = jsonPlaceHolderApi.putPost(5,post);  // put olduğunda içerik tamamen girildiği şekilde değişir. girilmeyenler null olur
+       // Call<Post> call = jsonPlaceHolderApi.patchPost(5,post); // patch olduğunda ise içerikte sadece girilen değerler değişir. girilmeyenler eskiden neyse o kalır
+
+        call.enqueue(new Callback<Post>() {
+            @Override
+            public void onResponse(Call<Post> call, Response<Post> response) {
+                if(!response.isSuccessful()){
+                    textViewResult.setText("Code: "+response.code());
+                    return;
+                }
+                Post postResponse = response.body();
+                String content = "";
+                content+="Code: "+response.code()+"\n";  // code 200 OK üretir
+                content +="ID: "+postResponse.getId() + "\n";
+                content+="User ID: "+postResponse.getUserId() + "\n";
+                content+="Title: "+postResponse.getTitle()+ "\n";
+                content+="Text: "+postResponse.getText()+"\n\n";
+
+                textViewResult.setText(content);
+            }
+
+            @Override
+            public void onFailure(Call<Post> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void deletePost(){
+        Call<Void> call = jsonPlaceHolderApi.deletePost(5);
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                textViewResult.setText("Code: "+response.code());
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+}
